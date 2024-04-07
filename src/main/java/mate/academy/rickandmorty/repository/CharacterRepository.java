96d6c745f2967fb4abb0f7e8839665bb86736d96
Farion Oleksandr <sashafarion@gmail.com>
2024-04-07 16:51:55 +0300

    added minor fixes

--- src/main/java/mate/academy/rickandmorty/repository/CharacterRepository.java
+++ src/main/java/mate/academy/rickandmorty/repository/CharacterRepository.java
@@ -3,14 +3,7 @@ package mate.academy.rickandmorty.repository;
 import java.util.List;
 import mate.academy.rickandmorty.model.Character;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface CharacterRepository extends JpaRepository<Character, Long> {
-    List<Character> findByNameLikeIgnoreCase(String name);
-
-    @Query("SELECT c " +
-            "FROM Character c " +
-            "WHERE UPPER(c.name) " +
-            "LIKE UPPER(CONCAT('%', ?1, '%'))")
     List<Character> findByNameContainsIgnoreCase(String name);
 }
